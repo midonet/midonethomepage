a1c1909ef6d67098a32acd2f0efbb7ff3e19be42
Kia King Ishii <kia.king.08@gmail.com>
2015-07-11 03:31:58 +0900

    Style the php code
    - Always use full opening php tag `<?php` never `<?`
    - Use single quite for string where possible (expect html attribute)
    - Consistent style with index.html

--- midonet-tv.php
+++ midonet-tv.php
@@ -1,36 +1,42 @@
 <?php
 require_once('_globals.php');
-$page=new Page("Midonet TV","View and Learn More about Midonet, Demonstrations, Cold Walkthroughs.");
+
+$page = new Page('Midonet TV', 'View and Learn More about Midonet, Demonstrations, Cold Walkthroughs.');
 $page->Queue()->addCSS('video-channel.css');
 $page->Queue()->addCSS('jquery-ui-midokura.css');
 
-?>
-
-<? /* page structure start-------------------------------------------------------- */
+/* Start Page Structure ----------------------------------------------------------- */
 include '_header.php';
 $page->Queue()->addFooterJS('video-player.js');
 $page->Queue()->addFooterJS('jquery-ui.js');
- ?>
-<? /* page Body Content ---------------------------------------------------------- */ ?>
 
+/* Start Page Body Content -------------------------------------------------------- */
+?>
 <section id="video-section">
-<div class="vdesc">
-<h1>MidoNet TV</h1><div id="video-nav">View: <ul>
-<li><a class="scrolltovideo" href="#demo">Demos</a></li>
-<li><a class="scrolltovideo" href="#codew">Code Walkthroughs</a></li>
-<li><a class="scrolltovideo" href="#education">Education</a></li>
-</ul></div>
-<h2 class="medium lighter">Watch and Learn from MidoNet Committers</h2><br/>
-If you have a video you want to share with the rest of the MidoNet community,<br/><em>join MidoNet on Slack and ping </em><span class="darker">@adjohn</span> or <span class="darker">@susanwu</span>
-</div>
+  <div class="vdesc">
+    <h1>MidoNet TV</h1>
+    <div id="video-nav">
+      View:
+      <ul>
+        <li><a class="scrolltovideo" href="#demo">Demos</a></li>
+        <li><a class="scrolltovideo" href="#codew">Code Walkthroughs</a></li>
+        <li><a class="scrolltovideo" href="#education">Education</a></li>
+      </ul>
+    </div>
+    <h2 class="medium lighter">Watch and Learn from MidoNet Committers</h2>
+    <br>
+    If you have a video you want to share with the rest of the MidoNet community,
+    <br>
+    <em>join MidoNet on Slack and ping</em> <span class="darker">@adjohn</span> or <span class="darker">@susanwu</span>
+  </div>
 
-<?php     renderVideoChannel('Demonstration Videos','demos',"demo"); ?>
-<?php     renderVideoChannel('Code Walkthroughs','code-walkthroughs',"codew"); ?>
-<?php     renderSlideChannel('Educational Slides','education',"education"); ?>
+  <?php renderVideoChannel('Demonstration Videos', 'demos', 'demo'); ?>
+  <?php renderVideoChannel('Code Walkthroughs', 'code-walkthroughs', 'codew'); ?>
+  <?php renderSlideChannel('Educational Slides', 'education', 'education'); ?>
 
-<div id="vplayer" ></div>
+  <div id="vplayer"></div>
 </section>
+<?php
+/* End Page Body Content ---------------------------------------------------------- */
 
-<? /* End page Body Content ---------------------------------------------------------- */ ?>
-
-<?include '_footer.php';?>
+include '_footer.php';
