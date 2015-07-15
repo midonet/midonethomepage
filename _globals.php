6e95f350976aaaa2fdd351cb8930334773cfeca3
Kia King Ishii <kia.king.08@gmail.com>
2015-07-16 03:27:05 +0900

    Apply PSR-2 coding style and PHPDocumentor commenting

--- _globals.php
+++ _globals.php
@@ -1,52 +1,69 @@
-<?php 
-/*
-Title: Global Php Code and Functions for midonet.org accessible to all pages.
-Author: Amit Talwar <amit@midokura.com>
-Client: Midokura SARL
-Copyright © 2015
-*/
+<?php
+
+/**
+ * Global PHP Code and Functions for midonet.org accessible to all pages.
+ *
+ * client: Midokura SARL
+ *
+ * @author Amit Talwar <amit@midokura.com>
+ * @copyright 2015 midonet.org
+ */
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once('class.assetqueue.php');
-require_once('class.page.php');
-require_once('class.videochannel.php');
-require_once ('class.slideshare.php');
-$page= new Page(); //initialize the page
+require_once 'class.assetqueue.php';
+require_once 'class.page.php';
+require_once 'class.videochannel.php';
+require_once 'class.slideshare.php';
+$page = new Page; // initialize the page
 
-function renderVideoChannel($title,$channel,$cssID){
-	$chan = new VideoChannel($channel);
-	if(!$chan->size() > 0) return '';
-	print '<div id="'.$cssID.'" class="video-channel-container">';
-	print '<div class="video-channel-title">'.$title.'</div>';
-	print '<div class="video-channel-contents">';
-	foreach($chan->listing() as $video)
-	{
-	print '<div class="video-item-container"><div class="video-item">';	
-	print "<img src='".$video->getThumbURL()."'".' class="video-thumb" width="220" title="'.$video->getTitle().'" data-url="'.$video->getEmbedUrl().'"/>';
-	print "</div>";
-	print "<div class='video-item-title'>".$video->getTitle()."</div>";
-	print '</div>';	
-	}
-	print ('<div class="clearfix"> </div></div></div>');
+function renderVideoChannel($title, $channel, $cssID)
+{
+    $chan = new VideoChannel($channel);
+
+    if ( ! $chan->size() > 0) {
+        return '';
+    }
+
+    print '<div id="'.$cssID.'" class="video-channel-container">';
+    print '<div class="video-channel-title">'.$title.'</div>';
+    print '<div class="video-channel-contents">';
+
+    foreach ($chan->listing() as $video) {
+        print '<div class="video-item-container">';
+        print '<div class="video-item">';
+        print '<img src="'.$video->getThumbURL().'" class="video-thumb" width="220" title="'.$video->getTitle().'" data-url="'.$video->getEmbedUrl().'">';
+        print '</div>';
+        print '<div class="video-item-title">'.$video->getTitle().'</div>';
+        print '</div>';
+    }
+
+    print ('<div class="clearfix"></div>');
+    print '</div>';
+    print '</div>';
 }
 
-function renderSlideChannel($title,$channel,$cssID){
-	$chan = new SlideShare($channel);
-	if(!$chan->size() > 0) return '';
-	print '<div id="'.$cssID.'" class="video-channel-container">';
-	print '<div class="video-channel-title">'.$title.'</div>';
-	print '<div class="video-channel-contents">';
-	foreach($chan->listing() as $video)
-	{
-	print '<div class="video-item-container"><div class="video-item">';	
-	print "<img src='".$video->getThumbURL()."'".' class="video-thumb" width="220" title="'.$video->getTitle().'" data-url="'.$video->getEmbedUrl().'"/>';
-	print "</div>";
-	print "<div class='video-item-title'>".$video->getTitle()."</div>";
-	print '</div>';	
-	}
-	print ('<div class="clearfix"> </div></div></div>');
+function renderSlideChannel($title, $channel, $cssID)
+{
+    $chan = new SlideShare($channel);
+
+    if ( ! $chan->size() > 0) {
+        return '';
+    }
+
+    print '<div id="'.$cssID.'" class="video-channel-container">';
+    print '<div class="video-channel-title">'.$title.'</div>';
+    print '<div class="video-channel-contents">';
+
+    foreach($chan->listing() as $video) {
+        print '<div class="video-item-container"><div class="video-item">';
+        print '<img src="'.$video->getThumbURL().'" class="video-thumb" width="220" title="'.$video->getTitle().'" data-url="'.$video->getEmbedUrl().'">';
+        print "</div>";
+        print "<div class='video-item-title'>".$video->getTitle()."</div>";
+        print '</div>';
+    }
+
+    print ('<div class="clearfix"></div>');
+    print '</div>';
+    print '</div>';
 }
-
-
-
-?>
--- class.slideshare.php
+++ class.slideshare.php
@@ -1,94 +1,104 @@
 <?php
-/*
-Title: SlideShare  Channel class
-Description: Provides base for maintaining Slideshare Slides
-			Uses a Tab delimited  text file in channels directory with a text file with extension .sxt for each channel in the format
-			ID(slide id),Title[title to slide],thumbnail Url,Link (direct link to slide)
-Author: Amit Talwar <amit@midokura.com>
-Client: Midokura SARL
-Copyright © 2015
 
-*/
+/**
+ * SlideShare Channel class.
+ *
+ * Provides base for maintaining Slideshare Slides Uses a Tab delimited text
+ * file in channels directory with a text file with extension .sxt for each
+ * channel in the format ID (slide id), Title (title to slide), thumbnail Url,
+ * Link (direct link to slide).
+ *
+ * Client: Midokura SARL
+ *
+ * @author Amit Talwar <amit@midokura.com>
+ * @copyright 2015 midonet.org
+ */
 
 class SlideShare {
-	protected $count=0;
-	protected $counter=0;
-	protected $fp;
+    protected $count=0;
+    protected $counter=0;
+    protected $fp;
 
-	function SlideShare($channel){
-		$filename = __DIR__.'/channels/'.$channel.'.sxt';
+	public function SlideShare($channel)
+    {
+        $filename = __DIR__.'/channels/'.$channel.'.sxt';
 
-		if (!file_exists ($filename))
-		{
-			die("Channel " .$channel." Does not exist!!");
-		}
-		$this->fp=file($filename);
-		$this->count = count($this->fp);
-	}
+        if ( ! file_exists ($filename)) {
+            die('Channel ' .$channel.' Does not exist!!');
+        }
+
+        $this->fp = file($filename);
+        $this->count = count($this->fp);
+    }
 
 	public function size()
 	{
 		return $this->count;
 	}
 
-	public function listing() { //returns an array of videos
-		$varr=array();
-		for($i=0; $i<$this->count; $i++) {
-			$row=explode('	',$this->fp[$i]);
-		//	$ft=($row[3]==1)?true:false;
-			$video=new SlideShow($row[0],$row[1],$row[2],$row[3]);
-			$varr[]= $video;
+    // Returns an array of videos.
+	public function listing()
+    {
+        $varr = array();
 
-		}
-		return $varr;
+        for ($i = 0; $i < $this->count; $i++) {
+			$row = explode("\t", $this->fp[$i]);
+            $video = new SlideShow($row[0], $row[1], $row[2], $row[3]);
+            $varr[] = $video;
+        }
 
-	}
-
+        return $varr;
+    }
 }
 
-class SlideShow {
-	protected $id='';
-	protected $title='';
-	protected $thumb='';
-	protected $link='';
+class SlideShow
+{
+    protected $id = '';
+    protected $title = '';
+    protected $thumb = '';
+    protected $link = '';
+    protected $featured = false;
 
-	protected $featured=false;
+    public function SlideShow($id, $title, $thumb, $link)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->thumb = $thumb;
+        $this->link = $link;
+    }
 
-	function SlideShow($id,$title,$thumb,$link)	{
-		$this->id = $id;
-		$this->title=$title;
-		$this->thumb = $thumb;
-		$this->link = $link;
+    public function getID()
+    {
+        return $this->id;
+    }
 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function isFeatured()
+    {
+        return $this->featured;
+    }
+
+    public function getPageURL()
+    {
+        return $this->link;
+    }
+
+    public function getThumbUrl()
+    {
+        return $this->thumb;
+    }
+
+	public function getEmbedUrl()
+    {
+		return '//www.slideshare.net/slideshow/embed_code/'.$this->id;
 	}
-
-	function getID()		{
-		return $this->id;
-	}
-
-	function getTitle()		{
-		return $this->title;
-	}
-
-	function getDescription() {
-		return $this->description;
-	}
-	function isFeatured() {
-		return $this->featured;
-	}
-	function getPageURL(){
-		return $this->link;
-	}
-	function getThumbUrl() {
-		return $this->thumb;
-	}
-
-	function getEmbedUrl() {
-		return "//www.slideshare.net/slideshow/embed_code/".$this->id;
-
-	}
-
-
 }
-
-?>
