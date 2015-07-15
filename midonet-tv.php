c54d32603b72b7f8db009b71b183a6ef293bf2f0
Kia King Ishii <kia.king.08@gmail.com>
2015-07-16 01:53:30 +0900

    Redirect who ever access to midonet-tv.php

--- midonet-tv.php
+++ midonet-tv.php
@@ -1,42 +1,7 @@
 <?php
-require_once('_globals.php');
 
-$page = new Page('Midonet TV', 'View and Learn More about Midonet, Demonstrations, Cold Walkthroughs.');
-$page->Queue()->addCSS('video-channel.css');
-$page->Queue()->addCSS('jquery-ui-midokura.css');
-
-/* Start Page Structure ----------------------------------------------------------- */
-include '_header.php';
-$page->Queue()->addFooterJS('video-player.js');
-$page->Queue()->addFooterJS('jquery-ui.js');
-
-/* Start Page Body Content -------------------------------------------------------- */
-?>
-<section id="video-section">
-  <div class="vdesc">
-    <h1>MidoNet TV</h1>
-    <div id="video-nav">
-      View:
-      <ul>
-        <li><a class="scrolltovideo" href="#demo">Demos</a></li>
-        <li><a class="scrolltovideo" href="#codew">Code Walkthroughs</a></li>
-        <li><a class="scrolltovideo" href="#education">Education</a></li>
-      </ul>
-    </div>
-    <h2 class="medium lighter">Watch and Learn from MidoNet Committers</h2>
-    <br>
-    If you have a video you want to share with the rest of the MidoNet community,
-    <br>
-    <em>join MidoNet on Slack and ping</em> <span class="darker">@adjohn</span> or <span class="darker">@susanwu</span>
-  </div>
-
-  <?php renderVideoChannel('Demonstration Videos', 'demos', 'demo'); ?>
-  <?php renderVideoChannel('Code Walkthroughs', 'code-walkthroughs', 'codew'); ?>
-  <?php renderSlideChannel('Educational Slides', 'education', 'education'); ?>
-
-  <div id="vplayer"></div>
-</section>
-<?php
-/* End Page Body Content ---------------------------------------------------------- */
-
-include '_footer.php';
+// This file has been replaced to `midonet-tv/index.php` but kept here to keep
+// URL accessible for a while. It should be deleted in future but For now this
+// file will simply redirect user to `midonet-tv/index.php`.
+header('Location: /midonet-tv');
+exit();
