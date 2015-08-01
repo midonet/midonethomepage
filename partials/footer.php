6a408fada3db83ccb198fec6a80b5af443ee9645
Kia King Ishii <kia.king.08@gmail.com>
2015-08-02 01:57:00 +0900

    responsive ready footer
    Now footer section is responsive ready. Also added some animation
    to link hover.
    
    Change-Id: I549437ed8e390eb92cb19c772df03ff6420899d1

--- partials/footer.php
+++ partials/footer.php
@@ -1,22 +1,28 @@
 <section class="footer">
-  <div class="wrapper clearfix">
-    <div class="top clearfix">
-      <span class="logo">Midonet</span>
-      &copy; 2011 - 2015
-      <a href="/trademark-policy">Trademark Policy</a>
-      <a href="/privacy-policy">Privacy Policy</a>
+  <div class="footer__container clearfix">
+    <div class="footer__top clearfix">
+      <p class="logo-box"><span class="logo">Midonet</span></p>
+
+      <div class="meta-box">
+        <small>&copy; 2011 - 2015</small>
+        <a href="/trademark-policy">Trademark Policy</a>
+        <a href="/privacy-policy">Privacy Policy</a>
+      </div>
     </div>
-    <ul>
+
+    <ul class="first">
       <li><a href="https://github.com/midonet/midonet/wiki">Midonet Wiki</a></li>
       <li><a href="http://blog.midonet.org">MidoNet Blog</a></li>
       <li><a href="https://www.midonet.org/#help">Get Help</a></li>
       <li><a href="http://eepurl.com/65TFr">Get the Newsletter</a></li>
     </ul>
+
     <ul>
       <li><a href="http://midokura.com">Sponsored by Midokura</a></li>
       <li><a href="http://docs.midonet.org">Documentation</a></li>
       <li><a href="http://docs.midonet.org/docs/latest/rest-api/">API Specification</a></li>
     </ul>
+
     <ul class="social">
       <li class="twitter"><a href="https://twitter.com/midonet">@midonet</a></li>
       <li class="github"><a href="https://www.github.com/midonet">github.com/midonet</a></li>
