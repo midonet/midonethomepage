02744ac8abdde58ef0bed3ea329a04af13a2f074
Sandro Mathys <sandro@midokura.com>
2015-10-02 03:43:11 +0900

    New MidoDay Tokyo 2015 URL
    Change-Id: I7d0b9b39b435c3953feacbc7310609505dce5dc1

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
   <body>
 
     <div class="banner">
-      <p>Join us for the <a href="https://github.com/midonet/midonet/wiki/MidoNet-Mini-Summit-Tokyo-2015">MidoDay Tokyo 2015</a></p>
+      <p>Join us for the <a href="https://midoday.midonet.org/tokyo-2015">MidoDay Tokyo 2015</a></p>
     </div>
 
     <section class="header">
